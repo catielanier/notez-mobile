e280ae73bee49235e700cc2cf7c3be0fce6bb2f8
Corey Lanier <iam@coreylanier.com>
2020-04-22 21:44:21 -0400

    created navigation

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { StyleSheet } from "react-native";
 import { Provider as PaperProvider, DefaultTheme } from "react-native-paper";
 import LanguageContextProvider from "./contexts/LanguageContext";
 import ComponentContainer from "./ComponentContainer";
@@ -25,12 +24,3 @@ export default function App() {
     </PaperProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
--- ComponentContainer.js
+++ ComponentContainer.js
@@ -1,11 +1,24 @@
+import "react-native-gesture-handler";
 import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 import { View } from "react-native";
 import Header from "./components/Header";
+import Home from "./components/Home";
+import Login from "./components/Login";
+
+const Stack = createStackNavigator();
 
 export default function ComponentContainer() {
   return (
     <View>
       <Header />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="Login" component={Login} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </View>
   );
 }
