e9bdf256644d0e9359381c1dd86931eba7b263bf
Corey Lanier <iam@coreylanier.com>
2020-04-22 22:23:12 -0400

    created ternary for changing home on valid user

--- ComponentContainer.js
+++ ComponentContainer.js
@@ -1,21 +1,29 @@
 import "react-native-gesture-handler";
-import React from "react";
+import React, { useContext } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { View } from "react-native";
 import Header from "./components/Header";
 import Home from "./components/Home";
 import Login from "./components/Login";
+import GameNotes from "./components/GameNotes";
+import { UserContext } from "./contexts/UserContext";
 
 const Stack = createStackNavigator();
 
 export default function ComponentContainer() {
+  const { user } = useContext(UserContext);
   return (
     <View>
-      <Header />
       <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Home" component={Home} />
+        <Header />
+        <Stack.Navigator initialRouteName="Home">
+          {user ? (
+            <Stack.Screen name="Home" component={GameNotes} />
+          ) : (
+            <Stack.Screen name="Home" component={Home} />
+          )}
+
           <Stack.Screen name="Login" component={Login} />
         </Stack.Navigator>
       </NavigationContainer>
