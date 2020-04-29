8a6f600b54f37e751d31ef86ee31eeefe9a26578
Corey Lanier <iam@coreylanier.com>
2020-04-29 17:05:07 -0400

    created base signin form

--- components/Login.js
+++ components/Login.js
@@ -1,10 +1,33 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState, useContext } from "react";
+import { View } from "react-native";
+import { Title, TextInput, Button } from "react-native-paper";
+import { LanguageContext } from "../contexts/LanguageContext";
+import localeSelect from "../services/localeSelect";
+import {
+  email as emailLocale,
+  login,
+  password as passwordLocale,
+} from "../data/locales";
 
 export default function Login() {
+  const { language } = useContext(LanguageContext);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   return (
     <View>
-      <Text>Login</Text>
+      <Title>{localeSelect(language, login)}</Title>
+      <TextInput
+        label={localeSelect(language, emailLocale)}
+        value={email}
+        onChangeText={(text) => setEmail(text)}
+      />
+      <TextInput
+        label={localeSelect(language, passwordLocale)}
+        value={password}
+        onChangeText={(text) => setPassword(text)}
+        secureTextEntry
+      />
+      <Button mode="contained">{localeSelect(language, login)}</Button>
     </View>
   );
 }
