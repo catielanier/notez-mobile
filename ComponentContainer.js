66ed855654ff7f40ec8fc0ce2a0536153575cb16
Corey Lanier <iam@coreylanier.com>
2020-04-29 16:29:34 -0400

    created menu context

--- ComponentContainer.js
+++ ComponentContainer.js
@@ -2,31 +2,32 @@ import "react-native-gesture-handler";
 import React, { useContext } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import { View } from "react-native";
 import Header from "./components/Header";
 import Home from "./components/Home";
 import Login from "./components/Login";
 import GameNotes from "./components/GameNotes";
 import { UserContext } from "./contexts/UserContext";
+import MenuContextProvider from "./contexts/MenuContext";
 
 const Stack = createStackNavigator();
 
 export default function ComponentContainer() {
   const { user } = useContext(UserContext);
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="Home"
-        screenOptions={{ header: Header }}
-      >
-        {user ? (
-          <Stack.Screen name="Home" component={GameNotes} />
-        ) : (
-          <Stack.Screen name="Home" component={Home} />
-        )}
-
-        <Stack.Screen name="Login" component={Login} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <MenuContextProvider>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="Home"
+          screenOptions={{ header: Header }}
+        >
+          {user ? (
+            <Stack.Screen name="Home" component={GameNotes} />
+          ) : (
+            <Stack.Screen name="Home" component={Home} />
+          )}
+          <Stack.Screen name="Login" component={Login} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </MenuContextProvider>
   );
 }
