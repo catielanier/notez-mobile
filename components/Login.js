7947b5ef9981349cece333d4114489706c2319a6
Corey Lanier <iam@coreylanier.com>
2020-04-30 21:10:19 -0400

    sort of styled

--- components/Login.js
+++ components/Login.js
@@ -1,5 +1,5 @@
 import React, { useState, useContext } from "react";
-import { View } from "react-native";
+import { View, StyleSheet, KeyboardAvoidingView } from "react-native";
 import { Title, TextInput, Button } from "react-native-paper";
 
 // Contexts
@@ -14,36 +14,57 @@ import {
   password as passwordLocale,
 } from "../data/locales";
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  inner: {
+    padding: 20,
+    paddingBottom: 320,
+    flex: 1,
+    justifyContent: "space-around",
+  },
+  textInput: {
+    height: 40,
+  },
+});
+
 export default function Login() {
   const { language } = useContext(LanguageContext);
   const { doLogin, loading } = useContext(UserContext);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   return (
-    <View>
-      <Title>{localeSelect(language, login)}</Title>
-      <TextInput
-        label={localeSelect(language, emailLocale)}
-        value={email}
-        onChangeText={(text) => setEmail(text)}
-      />
-      <TextInput
-        label={localeSelect(language, passwordLocale)}
-        value={password}
-        onChangeText={(text) => setPassword(text)}
-        secureTextEntry
-      />
-      <Button
-        mode="contained"
-        uppercase
-        loading={loading}
-        onPress={() => {
-          setEmail(email.toLowerCase);
-          doLogin(email, password);
-        }}
-      >
-        {localeSelect(language, login)}
-      </Button>
-    </View>
+    <KeyboardAvoidingView style={styles.container}>
+      <View style={styles.inner}>
+        <Title>{localeSelect(language, login)}</Title>
+        <TextInput
+          label={localeSelect(language, emailLocale)}
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+          style={styles.container}
+          mode="outlined"
+        />
+        <TextInput
+          label={localeSelect(language, passwordLocale)}
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+          secureTextEntry
+          style={styles.container}
+          mode="outlined"
+        />
+        <Button
+          mode="contained"
+          uppercase
+          loading={loading}
+          onPress={() => {
+            setEmail(email.toLowerCase);
+            doLogin(email, password);
+          }}
+        >
+          {localeSelect(language, login)}
+        </Button>
+      </View>
+    </KeyboardAvoidingView>
   );
 }
--- components/Signup.js
+++ components/Signup.js
@@ -1,19 +1,65 @@
 import React, { useContext, useState } from "react";
-import { View } from "react-native";
+import {
+  KeyboardAvoidingView,
+  StyleSheet,
+  View,
+  TouchableWithoutFeedback,
+  Platform,
+  Keyboard,
+  ScrollView,
+} from "react-native";
 import { Title, TextInput, Button } from "react-native-paper";
+import SearchableDropdown from "react-native-searchable-dropdown";
 
 import { LanguageContext } from "../contexts/LanguageContext";
 import { UserContext } from "../contexts/UserContext";
 
 import localeSelect from "../services/localeSelect";
+import dbLocale from "../services/dbLocale";
 import {
   email as emailLocale,
   signup,
   password as passwordLocale,
+  verifyPassword as verifyPasswordLocale,
+  username as usernameLocale,
+  realName as realNameLocale,
+  country as countryLocale,
 } from "../data/locales";
+import countries from "../data/countries";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  inner: {
+    padding: 20,
+    flex: 1,
+    justifyContent: "space-around",
+  },
+  textInput: {
+    height: 40,
+  },
+  searchableDropdown: {
+    height: 40,
+    borderColor: "#333",
+    borderWidth: 1,
+    borderRadius: 5,
+    paddingHorizontal: 12,
+    marginVertical: 5,
+    fontSize: 16,
+  },
+  itemContainer: {
+    maxHeight: 140,
+  },
+  itemStyle: {
+    paddingHorizontal: 12,
+    backgroundColor: "#aaa",
+    paddingVertical: 30,
+  },
+});
 
 export default function Signup() {
-  const { signup: doSignup } = useContext(UserContext);
+  const { signup: doSignup, loading, success } = useContext(UserContext);
   const { language } = useContext(LanguageContext);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -21,9 +67,82 @@ export default function Signup() {
   const [username, setUsername] = useState("");
   const [realName, setRealName] = useState("");
   const [country, setCountry] = useState("");
+  const countryOptions = [];
+  countries.map((item) => {
+    const value = {
+      value: JSON.stringify(item.value),
+      name: dbLocale(language, item),
+    };
+    countryOptions.push(value);
+  });
   return (
-    <View>
-      <Title>{localeSelect(language, signup)}</Title>
+    <View style={styles.inner}>
+      <ScrollView>
+        <Title>{localeSelect(language, signup)}</Title>
+        <TextInput
+          label={localeSelect(language, emailLocale)}
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+          style={styles.textInput}
+          mode="outlined"
+        />
+        <TextInput
+          label={localeSelect(language, passwordLocale)}
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+          secureTextEntry
+          style={styles.textInput}
+          mode="outlined"
+        />
+        <TextInput
+          label={localeSelect(language, verifyPasswordLocale)}
+          value={verifyPassword}
+          onChangeText={(text) => setVerifyPassword(text)}
+          secureTextEntry
+          style={styles.textInput}
+          mode="outlined"
+        />
+        <TextInput
+          label={localeSelect(language, usernameLocale)}
+          value={username}
+          onChangeText={(text) => setUsername(text)}
+          style={styles.textInput}
+          mode="outlined"
+        />
+        <TextInput
+          label={localeSelect(language, realNameLocale)}
+          value={realName}
+          onChangeText={(text) => setRealName(text)}
+          style={styles.textInput}
+          mode="outlined"
+        />
+        <SearchableDropdown
+          placeholder={localeSelect(language, countryLocale)}
+          textInputStyle={styles.searchableDropdown}
+          itemStyle={{
+            paddingHorizontal: 12,
+            backgroundColor: "#fff",
+            paddingVertical: 12,
+          }}
+          itemsContainerStyle={{ maxHeight: 240 }}
+          items={countryOptions}
+          onItemSelect={(item) => {
+            console.log(item);
+          }}
+          textInputProps={{ textInputChange: (text) => alert(text) }}
+        />
+        <Button
+          mode="contained"
+          uppercase
+          loading={loading}
+          onPress={() => {
+            setEmail(email.toLowerCase);
+            doSignup(email, password);
+          }}
+        >
+          {localeSelect(language, signup)}
+        </Button>
+      </ScrollView>
     </View>
   );
 }
