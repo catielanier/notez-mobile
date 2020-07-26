c28f668799e74edff59720e25d10fd3a62ffefb5
Corey Lanier <iam@coreylanier.com>
2020-07-25 20:31:43 -0400

    created submission for login

--- components/Login.js
+++ components/Login.js
@@ -1,12 +1,17 @@
 import React, { useState, useContext } from "react";
 import { View, Text, TextInput } from "react-native";
 import { UserContext } from "../contexts/UserContext";
+import PrimaryButton from "../elements/PrimaryButton";
 import { tailwind } from "../lib/tailwind";
+import localeSelect from "../services/localeSelect";
+import { LanguageContext } from "../contexts/LanguageContext";
+import { login } from "../data/locales";
 
 export default function Login({ navigation }) {
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
 	const { doLogin, success, error } = useContext(UserContext);
+	const { language } = useContext(LanguageContext);
 	if (success) {
 		return navigation.navigate("Home");
 	}
@@ -27,6 +32,12 @@ export default function Login({ navigation }) {
 				}}
 				secureTextEntry
 				placeholder="Password"
+			/>
+			<PrimaryButton
+				onPress={() => {
+					doLogin(email, password);
+				}}
+				text={localeSelect(language, login)}
 			/>
 		</View>
 	);
