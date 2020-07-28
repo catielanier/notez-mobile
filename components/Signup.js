b1ad95896865df3e4c6fcdd03e275779e7292aa8
Corey Lanier <iam@coreylanier.com>
2020-07-27 23:36:00 -0400

    added all fields but country, may do multipart form later

--- components/Signup.js
+++ components/Signup.js
@@ -1,5 +1,10 @@
 import React, { useContext, useState } from "react";
-import { KeyboardAvoidingView, Text, TextInput } from "react-native";
+import {
+	ActivityIndicator,
+	KeyboardAvoidingView,
+	Text,
+	TextInput,
+} from "react-native";
 import PaddedView from "../elements/PaddedView";
 import { tailwind, getColor } from "../lib/tailwind";
 import countries from "../data/countries";
@@ -11,6 +16,9 @@ import {
 	email as emailLocale,
 	password as passwordLocale,
 	verifyPassword as verifyPasswordLocale,
+	username as usernameLocale,
+	realName as realNameLocale,
+	country as countryLocale,
 } from "../data/locales";
 
 export default function Signup() {
@@ -19,34 +27,59 @@ export default function Signup() {
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
 	const [verifyPassword, setVerifyPassword] = useState("");
+	const [username, setUsername] = useState("");
+	const [realName, setRealName] = useState("");
+	const [country, setCountry] = useState("");
 	return (
 		<PaddedView>
-			<KeyboardAvoidingView>
-				<Text style={tailwind("text-lg text-center")}>
-					{localeSelect(language, signup)}
-				</Text>
-				<TextInput
-					value={email}
-					onChangeText={(text) => {
-						setEmail(text);
-					}}
-					placeholder={localeSelect(language, emailLocale)}
+			<Text style={tailwind("text-lg text-center")}>
+				{localeSelect(language, signup)}
+				<ActivityIndicator
+					animating={loading}
+					size="small"
+					color={getColor("orange-500")}
 				/>
-				<TextInput
-					value={password}
-					onChangeText={(text) => {
-						setPassword(text);
-					}}
-					placeholder={localeSelect(language, passwordLocale)}
-				/>
-				<TextInput
-					value={verifyPassword}
-					onChangeText={(text) => {
-						setVerifyPassword(text);
-					}}
-					placeholder={localeSelect(language, verifyPasswordLocale)}
-				/>
-			</KeyboardAvoidingView>
+			</Text>
+			<TextInput
+				value={email}
+				onChangeText={(text) => {
+					setEmail(text);
+				}}
+				placeholder={localeSelect(language, emailLocale)}
+				style={tailwind("py-2 px-3 mx-3 my-1 border-b border-black")}
+			/>
+			<TextInput
+				value={password}
+				onChangeText={(text) => {
+					setPassword(text);
+				}}
+				placeholder={localeSelect(language, passwordLocale)}
+				style={tailwind("py-2 px-3 mx-3 my-1 border-b border-black")}
+			/>
+			<TextInput
+				value={verifyPassword}
+				onChangeText={(text) => {
+					setVerifyPassword(text);
+				}}
+				placeholder={localeSelect(language, verifyPasswordLocale)}
+				style={tailwind("py-2 px-3 mx-3 my-1 border-b border-black")}
+			/>
+			<TextInput
+				value={username}
+				onChangeText={(text) => {
+					setUsername(text);
+				}}
+				placeholder={localeSelect(language, usernameLocale)}
+				style={tailwind("py-2 px-3 mx-3 my-1 border-b border-black")}
+			/>
+			<TextInput
+				value={realName}
+				onChangeText={(text) => {
+					setRealName(text);
+				}}
+				placeholder={localeSelect(language, realNameLocale)}
+				style={tailwind("py-2 px-3 mx-3 my-1 border-b border-black")}
+			/>
 		</PaddedView>
 	);
 }
