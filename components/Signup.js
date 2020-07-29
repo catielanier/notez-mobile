65e66b619b4a08947faa30d3b6c28e9148696d27
Corey Lanier <iam@coreylanier.com>
2020-07-29 17:36:19 -0400

    created multipart  signup form

--- components/Signup.js
+++ components/Signup.js
@@ -4,12 +4,16 @@ import {
 	KeyboardAvoidingView,
 	Text,
 	TextInput,
+	View,
 } from "react-native";
-import PaddedView from "../elements/PaddedView";
+import DropDownPicker from "react-native-dropdown-picker";
+import PrimaryButton from "../elements/PrimaryButton";
+import SecondaryButton from "../elements/SecondaryButton";
 import { tailwind, getColor } from "../lib/tailwind";
 import countries from "../data/countries";
 import { UserContext } from "../contexts/UserContext";
 import localeSelect from "../services/localeSelect";
+import dbLocale from "../services/dbLocale";
 import { LanguageContext } from "../contexts/LanguageContext";
 import {
 	signup,
@@ -19,6 +23,8 @@ import {
 	username as usernameLocale,
 	realName as realNameLocale,
 	country as countryLocale,
+	next,
+	goBack,
 } from "../data/locales";
 
 export default function Signup() {
@@ -30,8 +36,9 @@ export default function Signup() {
 	const [username, setUsername] = useState("");
 	const [realName, setRealName] = useState("");
 	const [country, setCountry] = useState("");
+	const [formPart, setFormPart] = useState(1);
 	return (
-		<PaddedView>
+		<KeyboardAvoidingView style={tailwind("pt-3 px-2")}>
 			<Text style={tailwind("text-lg text-center")}>
 				{localeSelect(language, signup)}
 				<ActivityIndicator
@@ -40,46 +47,106 @@ export default function Signup() {
 					color={getColor("orange-500")}
 				/>
 			</Text>
-			<TextInput
-				value={email}
-				onChangeText={(text) => {
-					setEmail(text);
-				}}
-				placeholder={localeSelect(language, emailLocale)}
-				style={tailwind("py-2 px-3 mx-3 my-1 border-b border-black")}
-			/>
-			<TextInput
-				value={password}
-				onChangeText={(text) => {
-					setPassword(text);
-				}}
-				placeholder={localeSelect(language, passwordLocale)}
-				style={tailwind("py-2 px-3 mx-3 my-1 border-b border-black")}
-			/>
-			<TextInput
-				value={verifyPassword}
-				onChangeText={(text) => {
-					setVerifyPassword(text);
-				}}
-				placeholder={localeSelect(language, verifyPasswordLocale)}
-				style={tailwind("py-2 px-3 mx-3 my-1 border-b border-black")}
-			/>
-			<TextInput
-				value={username}
-				onChangeText={(text) => {
-					setUsername(text);
-				}}
-				placeholder={localeSelect(language, usernameLocale)}
-				style={tailwind("py-2 px-3 mx-3 my-1 border-b border-black")}
-			/>
-			<TextInput
-				value={realName}
-				onChangeText={(text) => {
-					setRealName(text);
-				}}
-				placeholder={localeSelect(language, realNameLocale)}
-				style={tailwind("py-2 px-3 mx-3 my-1 border-b border-black")}
-			/>
-		</PaddedView>
+			{formPart === 1 && (
+				<>
+					<TextInput
+						value={email}
+						onChangeText={(text) => {
+							setEmail(text);
+						}}
+						placeholder={localeSelect(language, emailLocale)}
+						style={tailwind("py-2 px-3 mx-3 my-4 border-b border-black")}
+						autoCapitalize="none"
+					/>
+					<TextInput
+						value={password}
+						onChangeText={(text) => {
+							setPassword(text);
+						}}
+						placeholder={localeSelect(language, passwordLocale)}
+						style={tailwind("py-2 px-3 mx-3 my-4 border-b border-black")}
+						autoCapitalize="none"
+					/>
+					<TextInput
+						value={verifyPassword}
+						onChangeText={(text) => {
+							setVerifyPassword(text);
+						}}
+						placeholder={localeSelect(language, verifyPasswordLocale)}
+						style={tailwind("py-2 px-3 mx-3 my-4 mb-8 border-b border-black")}
+					/>
+				</>
+			)}
+			{formPart === 2 && (
+				<>
+					<TextInput
+						value={username}
+						onChangeText={(text) => {
+							setUsername(text);
+						}}
+						placeholder={localeSelect(language, usernameLocale)}
+						style={tailwind("py-2 px-3 mx-3 my-4 border-b border-black")}
+					/>
+					<TextInput
+						value={realName}
+						onChangeText={(text) => {
+							setRealName(text);
+						}}
+						placeholder={localeSelect(language, realNameLocale)}
+						style={tailwind("py-2 px-3 mx-3 my-4 mb-8 border-b border-black")}
+					/>
+				</>
+			)}
+			{formPart === 3 && (
+				<DropDownPicker
+					items={countries.map((item) => {
+						return { value: item.value, label: dbLocale(language, item) };
+					})}
+					containerStyle={tailwind("h-10 mx-3 my-4 mb-8")}
+					dropDownStyle={tailwind("bg-white")}
+					onChangeItem={(item) => setCountry(item.value)}
+					itemStyle={{ justifyContent: "flex-start" }}
+					searchable={true}
+					searchablePlaceholder="Search"
+				/>
+			)}
+			<View style={tailwind("flex flex-row justify-start")}>
+				{formPart > 1 && (
+					<View style={tailwind("mr-2")}>
+						<SecondaryButton
+							text={localeSelect(language, goBack)}
+							onPress={() => {
+								setFormPart(formPart - 1);
+							}}
+						/>
+					</View>
+				)}
+				<View style={tailwind("mr-2")}>
+					<PrimaryButton
+						text={
+							formPart < 3
+								? localeSelect(language, next)
+								: localeSelect(language, signup)
+						}
+						onPress={() => {
+							if (formPart < 3) {
+								setFormPart(formPart + 1);
+							}
+							if (formPart === 3) {
+								setEmail(email.toLowerCase());
+								doSignup(
+									email,
+									password,
+									verifyPassword,
+									username,
+									realName,
+									country
+								);
+							}
+						}}
+					/>
+				</View>
+			</View>
+		</KeyboardAvoidingView>
 	);
 }
