6239bb53c987a9cf309e4ee785be6b94302464e7
Corey Lanier <iam@coreylanier.com>
2020-11-27 18:16:43 -0500

    fixed language detection in iOS 14

--- contexts/LanguageContext.js
+++ contexts/LanguageContext.js
@@ -4,26 +4,27 @@ import { Platform, NativeModules } from "react-native";
 export const LanguageContext = createContext();
 
 const LanguageContextProvider = (props) => {
-  const [language, setLanguage] = useState(null);
-  useEffect(() => {
-    const locale =
-      Platform.OS === "ios"
-        ? NativeModules.SettingsManager.settings.AppleLocale
-        : NativeModules.I18nManager.localeIdentifier;
-    const index = locale.indexOf("zh");
-    if (index !== 0) {
-      const userLanguage = locale.split(/[-_]/)[0];
-      setLanguage(userLanguage);
-    } else {
-      const userLanguage = locale;
-      setLanguage(userLanguage);
-    }
-  }, [language]);
-  return (
-    <LanguageContext.Provider value={{ language }}>
-      {props.children}
-    </LanguageContext.Provider>
-  );
+	const [language, setLanguage] = useState(null);
+	useEffect(() => {
+		const locale =
+			Platform.OS === "ios"
+				? NativeModules.SettingsManager.settings.AppleLocale ||
+				  NativeModules.SettingsManager.settings.AppleLanguages[0]
+				: NativeModules.I18nManager.localeIdentifier;
+		const index = locale.indexOf("zh");
+		if (index !== 0) {
+			const userLanguage = locale.split(/[-_]/)[0];
+			setLanguage(userLanguage);
+		} else {
+			const userLanguage = locale;
+			setLanguage(userLanguage);
+		}
+	}, [language]);
+	return (
+		<LanguageContext.Provider value={{ language }}>
+			{props.children}
+		</LanguageContext.Provider>
+	);
 };
 
 export default LanguageContextProvider;
