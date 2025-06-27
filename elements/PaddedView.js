ce519c5be95d7da0115837bceb386d93787e54bf
Catie Lanier <hello@catielanier.ca>
2025-06-26 20:51:40 -0400

    prep refactor in new rn version

--- elements/PaddedView.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { View } from "react-native";
-import { tailwind } from "../lib/tailwind";
-
-export default function PaddedView({ children }) {
-	return <View style={tailwind("pt-3 px-2")}>{children}</View>;
-}
--- i18n.js
+++
@@ -1,8 +0,0 @@
-import i18n from "i18next";
-import { initReactI18next } from "react-i18next";
-import { getLocales } from "react-native-localize";
-
-i18n.use(initReactI18next).init({
-	lng: getLocales()[0].languageCode,
-	fallbackLng: "en",
-});
